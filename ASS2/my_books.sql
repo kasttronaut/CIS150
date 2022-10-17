0b4d35125957f0850bf29ae8af3611ba5b0801e9
kasttronaut <112979566+kasttronaut@users.noreply.github.com>
2022-10-17 12:12:23 -0400

    Add files via upload

---
+++ ASS2/my_books.sql
@@ -0,0 +1,17 @@
+PRAGMA foreign_keys=OFF;
+
+BEGIN TRANSACTION;
+
+CREATE TABLE IF NOT EXISTS books(author TEXT, title TEXT, isbn TEXT PRIMARY KEY, publisher TEXT);
+
+INSERT INTO "books" VALUES('John Smith','Life','0-0-0-0-0-1','Bonnier');
+
+INSERT INTO "books" VALUES('James Woody','Love','0-0-0-0-0-2','Bonnier');
+
+INSERT INTO "books" VALUES('Joan Carmen','Guns','0-0-0-0-0-3','Bonnier');
+
+INSERT INTO "books" VALUES('Johnanna Boyd','Code','0-0-0-0-0-4','Bonnier');
+
+INSERT INTO "books" VALUES('Eva Peron','Cars','0-0-0-0-0-5','Books R us');
+
+COMMIT;
